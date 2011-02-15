c5f72354cefb5c2720069c6cfcb58ca7b9d5f01d
Tyler Kovacs <tyler@squareup.com>
2011-02-14 16:36:23 -0800

    change shoulda gem version to match what is installed on ci

--- test/helper.rb
+++ test/helper.rb
@@ -5,7 +5,7 @@ gem "activesupport", "= 2.3.10"
 gem "activerecord",  "= 2.3.10"
 gem "actionpack",    "= 2.3.10"
 gem "nokogiri",      "= 1.4.3.1"
-gem "shoulda",       "= 2.11.3"
+gem "shoulda",       ">= 2.11.1"
 gem 'bourne', '>= 1.0'
 gem "sham_rack",     "~> 1.3.0"
 
